0de851eec3420aab24c45c51917d9ec83578c284
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2024-08-29 14:55:33 -0400

    lots of fixes

--- src/flxsvg/FlxSvgSprite.hx
+++ src/flxsvg/FlxSvgSprite.hx
@@ -2,6 +2,7 @@ package flxsvg;
 
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.FlxG;
 import format.SVG;
 import format.svg.SVGRenderer;
 import openfl.display.BitmapData;
@@ -12,44 +13,48 @@ import openfl.utils.Assets;
  * A ```FlxSprite``` meant for loading SVG files via OpenFL's SVG rendering library.
  * @author Vortex
  */
-class FlxSvgSprite extends FlxSprite{
+class FlxSvgSprite extends FlxSprite {
+	private var _svg:SVG;
+	private var _shape:Shape;
+	private var _bitmapData:BitmapData;
 
-    private var _svg:SVG;
-    private var _shape:Shape;
+	/**
+	 * The raw SVG data as a string.
+	 */
+	public var data(default, null):String;
 
-    public var data:String;
+	/**
+	 * Creats a new ```FlxSvgSprite```
+	 * @param data The SVG data as a string.
+	 * @param X The X position.
+	 * @param Y The Y position.
+	 */
+	public function new(data:String, X:Float = 0, Y:Float = 0) {
+		super(X, Y);
+		this.data = data;
+		render(data);
+	}
 
-    /**
-     * Creats a new ```FlxSvgSprite```
-     * @param data The SVG data as a string.
-     * @param X The X position.
-     * @param Y The Y position.
-     */
-    public function new(data:String, X:Float = 0, Y:Float = 0){
-        super(X,Y);
-        this.data = data;
-        render(data);
-    }
-
-    public function render(data:String){
-        if(data == null || data.length <= 0){
-            data = Assets.getText("flxsvg/images/flixel.svg");
+	/**
+	 * Renders a SVG graphic to an FlxSprite
+	 * @param data 
+	 */
+	public function render(data:String) {
+        if (data == '' || (Xml.parse(data).firstElement()?.nodeName != "svg" && Xml.parse(data).firstElement()?.nodeName != "svg:svg")) {
+            FlxG.log.error("Not an SVG file ("+ (data == '' ? "null" : Xml.parse(data).firstElement()?.nodeName) + ")");
+            loadGraphic("flixel/images/logo/default.png");
+            return;
         }
-        _shape = new Shape();
-        _svg = new SVG(data);
-        _svg.render(_shape.graphics);
+		_shape = new Shape();
+		_svg = new SVG(data);
+		_svg.render(_shape.graphics);
 
-        var renderer = new SVGRenderer(_svg.data);
+		var renderer:SVGRenderer = new SVGRenderer(_svg.data);
 
-        var bitmapData = new BitmapData(Std.int(renderer.width), Std.int(renderer.height), true, FlxColor.TRANSPARENT);
-        bitmapData.draw(_shape);
-                
-        pixels = bitmapData;
-    }
+		_bitmapData = new BitmapData(Std.int(renderer.width), Std.int(renderer.height), true, FlxColor.TRANSPARENT);
+		_bitmapData.draw(_shape);
 
-    override function update(elapsed:Float) {
-        super.update(elapsed);
-        untyped pixels.width = width;
-        untyped pixels.height = height;
-    }
+		loadGraphic(_bitmapData);
+		updateHitbox();
+	}
 }
