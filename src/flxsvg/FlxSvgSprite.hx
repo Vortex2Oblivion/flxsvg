ea27ef46365d1b8d3a3c94161dc25b95049c4662
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-08 00:14:40 +0300

    Update FlxSvgSprite.hx

--- src/flxsvg/FlxSvgSprite.hx
+++ src/flxsvg/FlxSvgSprite.hx
@@ -9,18 +9,44 @@ import format.svg.SVGRenderer;
 import openfl.geom.Matrix;
 import openfl.display.Shape;
 
+/**
+ * A sprite that supports rendering Scalable Vector Graphics (SVG) within HaxeFlixel.
+ * This class uses OpenFL to render SVG content into a Flixel sprite, allowing for scalable,
+ * dynamic graphics in games or UI elements.
+ *
+ * @author Vortex2Oblivion
+ * @author Mihai Alexandru (MAJigsaw77)
+ */
 class FlxSvgSprite extends FlxSprite
 {
+	/**
+	 * The width of the SVG content to be rendered.
+	 */
 	public var svgWidth(default, set):Float = 0;
+
+	/**
+	 * The height of the SVG content to be rendered.
+	 */
 	public var svgHeight(default, set):Float = 0;
 
+	/**
+	 * Private storage for the parsed SVG data.
+	 */
 	@:noCompletion
 	private var svgData:SVGData;
 
+	/**
+	 * A flag indicating whether the sprite needs to be redrawn (if the SVG content, dimensions or antialiasing have changed).
+	 */
 	@:noCompletion
 	private var svgDirty:Bool = false;
 
-	public function new(x:Float = 0, y:Float = 0):Void
+	/**
+	 * Creates a `FlxSvgSprite` at a specified position.
+	 * @param x The initial X position of the sprite.
+	 * @param y The initial Y position of the sprite.
+	 */
+	public function new(?x:Float = 0, ?y:Float = 0):Void
 	{
 		super(x, y);
 	}
@@ -50,7 +76,7 @@ class FlxSvgSprite extends FlxSprite
 
 				new SVGRenderer(svgData).render(shape.graphics, matrix);
 
-				pixels.draw(shape);
+				pixels.draw(shape, antialiasing);
 
 				dirty = true;
 			}
@@ -61,17 +87,30 @@ class FlxSvgSprite extends FlxSprite
 		super.draw();
 	}
 
+	public override function destroy():Void
+	{
+		super.destroy();
+
+		svgData = null;
+	}
+
+	/**
+	 * Loads an SVG from a `FlxXmlAsset` and optionally sets its width and height.
+	 * @param svgData The `FlxXmlAsset` containing the SVG data.
+	 * @param svgWidth (Optional) The width to render the SVG. Defaults to the intrinsic width of the SVG.
+	 * @param svgHeight (Optional) The height to render the SVG. Defaults to the intrinsic height of the SVG.
+	 * @return This instance, to allow for method chaining.
+	 */
 	public function loadSvg(svgData:FlxXmlAsset, ?svgWidth:Float, ?svgHeight:Float):FlxSvgSprite
 	{
 		if (svgData != null)
 		{
 			final xmlData:Xml = svgData.getXml();
 
-			if (xmlData.firstElement()?.nodeName != "svg" && xmlData.firstElement()?.nodeName != "svg:svg")
+			if (xmlData.firstElement()?.nodeName != 'svg' && xmlData.firstElement()?.nodeName != 'svg:svg')
 				return this;
 
 			this.svgData = new SVGData(xmlData);
-
 			this.svgWidth = svgWidth > 0 ? svgWidth : this.svgData.width;
 			this.svgHeight = svgHeight > 0 ? svgHeight : this.svgData.height;
 
@@ -101,5 +140,16 @@ class FlxSvgSprite extends FlxSprite
 			svgDirty = true;
 
 		return svgHeight;
+	}
+
+	@:noCompletion
+	private override function set_antialiasing(value:Bool):Bool
+	{
+		antialiasing = value;
+
+		if (svgData != null)
+			svgDirty = true;
+
+		return antialiasing;
 	}
 }
