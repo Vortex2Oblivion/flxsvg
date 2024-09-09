05728ddcf010c212a595777ff1dcec0377d3627b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-09 16:09:09 +0000

    Rewrote `FlxSvgSprite`. (#1)

--- src/flxsvg/FlxSvgSprite.hx
+++ src/flxsvg/FlxSvgSprite.hx
@@ -1,59 +1,160 @@
 package flxsvg;
 
-import flixel.FlxSprite;
+import flixel.system.FlxAssets;
 import flixel.util.FlxColor;
 import flixel.FlxG;
-import format.SVG;
+import flixel.FlxSprite;
+import format.svg.SVGData;
 import format.svg.SVGRenderer;
-import openfl.display.BitmapData;
+import openfl.geom.Matrix;
 import openfl.display.Shape;
-import openfl.utils.Assets;
 
 /**
- * A ```FlxSprite``` meant for loading SVG files via OpenFL's SVG rendering library.
- * @author Vortex
+ * A sprite that supports rendering Scalable Vector Graphics (SVG) within HaxeFlixel.
+ * This class uses OpenFL to render SVG content into a Flixel sprite, allowing for scalable,
+ * dynamic graphics in games or UI elements.
+ *
+ * @author Vortex2Oblivion
+ * @author Mihai Alexandru (MAJigsaw77)
  */
-class FlxSvgSprite extends FlxSprite {
-	private var _svg:SVG;
-	private var _shape:Shape;
+class FlxSvgSprite extends FlxSprite
+{
+	/**
+	 * The width of the SVG content to be rendered.
+	 */
+	public var svgWidth(default, set):Float = 0;
 
 	/**
-	 * The raw SVG data as a string.
+	 * The height of the SVG content to be rendered.
 	 */
-	public var data(default, null):String;
+	public var svgHeight(default, set):Float = 0;
 
 	/**
-	 * Creats a new ```FlxSvgSprite```
-	 * @param data The SVG data as a string.
-	 * @param X The X position.
-	 * @param Y The Y position.
+	 * Private storage for the parsed SVG data.
 	 */
-	public function new(data:String, X:Float = 0, Y:Float = 0) {
-		super(X, Y);
-		this.data = data;
-		render(data);
+	@:noCompletion
+	private var svgData:SVGData;
+
+	/**
+	 * A flag indicating whether the sprite needs to be redrawn (if the SVG content, dimensions or antialiasing have changed).
+	 */
+	@:noCompletion
+	private var svgDirty:Bool = false;
+
+	/**
+	 * Creates a `FlxSvgSprite` at a specified position.
+	 * @param x The initial X position of the sprite.
+	 * @param y The initial Y position of the sprite.
+	 */
+	public function new(?x:Float = 0, ?y:Float = 0):Void
+	{
+		super(x, y);
+	}
+
+	public override function draw():Void
+	{
+		if (svgDirty)
+		{
+			if (svgData != null && pixels != null)
+			{
+				final diffX:Int = Math.floor(svgWidth) - pixels.width;
+				final diffY:Int = Math.floor(svgHeight) - pixels.height;
+
+				if (diffX != 0 || diffY != 0)
+					makeGraphic(Math.floor(svgWidth), Math.floor(svgHeight), FlxColor.TRANSPARENT, true);
+				else
+					pixels.fillRect(pixels.rect, FlxColor.TRANSPARENT);
+
+				final matrix:Matrix = new Matrix();
+
+				matrix.identity();
+
+				if (svgWidth > 0 && svgHeight > 0)
+					matrix.scale(Math.floor(svgWidth) / svgData.width, Math.floor(svgHeight) / svgData.height);
+
+				final shape:Shape = new Shape();
+
+				new SVGRenderer(svgData).render(shape.graphics, matrix);
+
+				pixels.draw(shape, antialiasing);
+
+				dirty = true;
+			}
+
+			svgDirty = false;
+		}
+
+		super.draw();
+	}
+
+	public override function destroy():Void
+	{
+		super.destroy();
+
+		svgData = null;
 	}
 
 	/**
-	 * Renders a SVG graphic to an FlxSprite
-	 * @param data The raw SVG data as a string.
-	 * Thanks to MAJigsaw for the code speedup!
+	 * Loads an SVG from a `FlxXmlAsset` and optionally sets its width and height.
+	 * @param svgData The `FlxXmlAsset` containing the SVG data.
+	 * @param svgWidth (Optional) The width to render the SVG. Defaults to the intrinsic width of the SVG.
+	 * @param svgHeight (Optional) The height to render the SVG. Defaults to the intrinsic height of the SVG.
+	 * @return This instance, to allow for method chaining.
 	 */
-	 public function render(data:String) {
-		// Protection for loading an invalid SVG file.
-        if (data == '' || (Xml.parse(data).firstElement()?.nodeName != "svg" && Xml.parse(data).firstElement()?.nodeName != "svg:svg")) {
-            FlxG.log.error("Not an SVG file ("+ (data == '' ? "null" : Xml.parse(data).firstElement()?.nodeName) + ")");
-            loadGraphic("flixel/images/logo/default.png");
-            return;
-        }
+	public function loadSvg(svgData:FlxXmlAsset, ?svgWidth:Float, ?svgHeight:Float):FlxSvgSprite
+	{
+		if (svgData != null)
+		{
+			final xmlData:Xml = svgData.getXml();
 
-        _shape = new Shape();
-        _svg = new SVG(data);
-        _svg.render(_shape.graphics);
+			if (xmlData.firstElement()?.nodeName != 'svg' && xmlData.firstElement()?.nodeName != 'svg:svg')
+			{
+				FlxG.log.error('Not an SVG file (${xmlData.firstElement()?.nodeName})');
+				return this;
+			}
 
-		// Draws the Graphic to the sprite.
-        final renderer:SVGRenderer = new SVGRenderer(_svg.data);
-        makeGraphic(Std.int(renderer.width), Std.int(renderer.height), FlxColor.TRANSPARENT);
-        pixels.draw(_shape);
-    }
+			this.svgData = new SVGData(xmlData);
+			this.svgWidth = svgWidth > 0 ? svgWidth : this.svgData.width;
+			this.svgHeight = svgHeight > 0 ? svgHeight : this.svgData.height;
+
+			makeGraphic(Math.floor(this.svgWidth), Math.floor(this.svgHeight), FlxColor.TRANSPARENT, true);
+		}
+		else
+			FlxG.log.error('No SVG data provided');
+
+		return this;
+	}
+
+	@:noCompletion
+	private function set_svgWidth(value:Float):Float
+	{
+		svgWidth = value;
+
+		if (svgData != null)
+			svgDirty = true;
+
+		return svgWidth;
+	}
+
+	@:noCompletion
+	private function set_svgHeight(value:Float):Float
+	{
+		svgHeight = value;
+
+		if (svgData != null)
+			svgDirty = true;
+
+		return svgHeight;
+	}
+
+	@:noCompletion
+	private override function set_antialiasing(value:Bool):Bool
+	{
+		antialiasing = value;
+
+		if (svgData != null)
+			svgDirty = true;
+
+		return antialiasing;
+	}
 }
